e48fcdea41e716f0cc15ae4363f98044d80248c8
zaherinfinity <vavannak364@gmail.com>
2026-05-17 13:31:32 +0700

    Delete topup_panel directory

--- topup_panel/static/js/main.js
+++
@@ -1,8 +0,0 @@
-function copyApiKey() {
-    const apiKey = document.getElementById('apiKey');
-    apiKey.select();
-    apiKey.setSelectionRange(0, 99999); // mobile
-    document.execCommand('copy');
-    // Optional toast
-    alert('API Key copied!');
-}
